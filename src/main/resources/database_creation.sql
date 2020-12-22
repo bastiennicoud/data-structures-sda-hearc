4a3ba8d44ff5db7e1a143be370d815a5e6de4ecc
Bastien <info@bastiennicoud.ch>
2020-12-22 18:54:48 +0100

    Add virtual table to provides a search index for the different resources of the platform.

--- src/main/resources/database_creation.sql
+++ src/main/resources/database_creation.sql
@@ -8,3 +8,27 @@ CREATE TABLE users (
 
 -- Index on users names
 CREATE INDEX users_names ON users (first_name, last_name);
+
+-- Search table for full text search on the indexed datas
+CREATE VIRTUAL TABLE full_text_search_index
+    USING FTS5(
+                  type, -- The human readable type (Ulilisateur, Cours...)
+                  title, -- The title of the resource (user name, course name)
+                  description, -- Some supplementary search friendly information
+                  resource_table_name UNINDEXED,
+                  resource_id UNINDEXED
+);
+
+SELECT *
+FROM full_text_search_index
+WHERE title MATCH 'test'
+ORDER BY rank;
+
+INSERT INTO full_text_search_index
+VALUES (
+        'Utilisateur',
+        'Bastien Nicoud',
+        'Status de lutilisateur',
+        'users',
+        '4'
+        );
