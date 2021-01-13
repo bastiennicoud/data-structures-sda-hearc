8614e67881c87569dd96d56a57797cd0a5aee6d8
Bastien <info@bastiennicoud.ch>
2021-01-13 12:08:22 +0100

    Add HttpHandler abstract class to regroup utility helpers.

--- src/main/java/http/SearchHandler.java
+++ src/main/java/http/SearchHandler.java
@@ -1,8 +1,8 @@
 package http;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
 import database.DataRepository;
 import database.exceptions.HydrationException;
 import entities.SearchResult;
@@ -15,54 +15,96 @@ import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.stream.Collectors;
 
-public class SearchHandler implements HttpHandler {
+/**
+ * Handles search calls on the /search endoint
+ * Return a json payload with the search results
+ */
+public class SearchHandler extends HttpHandler {
 
+    // Data repo to acces the DB
     private final DataRepository dataRepository;
 
+    // Mapper for json serialisation and deserialisation
     private final ObjectMapper mapper;
 
     public SearchHandler(DataRepository dataRepository) {
 
         this.dataRepository = dataRepository;
         this.mapper = new ObjectMapper();
+
     }
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
-        System.out.println("Search request");
+        System.out.println("Search request handler.");
 
-        var body = new BufferedReader(new InputStreamReader(exchange.getRequestBody()))
-                .lines().collect(Collectors.joining());
+        try (
+                // Open the input stream and try to get a buffer reader on the body
+                var bfReader = new BufferedReader(
+                        new InputStreamReader(exchange.getRequestBody())
+                )
+        ) {
 
-        System.out.println(body);
+            // Get the body payload
+            var body = bfReader.lines()
+                               .collect(Collectors.joining());
 
-        try {
+            System.out.println("Request body : " + body);
 
+            // Parse the json paylod
             var search = mapper.readTree(
                     body
             );
 
+            // Perform a text search on the DB with all the tokens of the needle
             var results = dataRepository.textSearch(
                     SearchResult.class,
                     search.get("needle").asText().split(" ")
             );
 
+            System.out.printf(
+                    "Request body %1$s",
+                    results.stream().map(r -> r.title + r.description).collect(Collectors.joining())
+            );
+
+            // serialise the search results
             var searchResponse = mapper.writeValueAsString(results)
                                        .getBytes(StandardCharsets.UTF_8);
 
+            // Get response headers and set the Content-Type
             var responseHeaders = exchange.getResponseHeaders();
             responseHeaders.set("Content-Type", "text/json; charset=UTF-8");
 
             exchange.sendResponseHeaders(200, searchResponse.length);
 
+            // Write rhe search results on the output stream
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(searchResponse);
             }
 
-        } catch (SQLException | HydrationException throwables) {
+        } catch (SQLException | HydrationException e) {
+
+            sendHttpError(
+                    exchange,
+                    500,
+                    "Error during database interactions."
+            );
+
             System.out.println("Error while making the search");
-            throwables.printStackTrace();
+            e.printStackTrace();
+
+        } catch (JsonProcessingException e) {
+
+            sendHttpError(
+                    exchange,
+                    500,
+                    "Error while performing json serialisation or deserialisation"
+            );
+
+            System.out.println("Error while parsing or serializing json");
+            e.printStackTrace();
+
         }
     }
 
